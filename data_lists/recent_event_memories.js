5d32dade6db0eb8e6bb8460d4c8670af998b41ca
Curie Club IISERM <curieclub@iisermohali.ac.in>
2024-08-02 22:27:35 +0530

    Update recent_event_memories.js

--- data_lists/recent_event_memories.js
+++ data_lists/recent_event_memories.js
@@ -1,26 +1,26 @@
 export const images = [
     {
-        src: "/assests/events_images/guess the element poster.webp",
+        src: "assests/events_images/guess the element poster.webp",
         alt: "Guess the Element Poster"
     },
     {
-        src: "/assests/events_images/curie-quiz-poster.webp",
+        src: "assests/events_images/curie-quiz-poster.webp",
         alt: "Curie Quiz Poster"
     },
     {
-        src: "/assests/events_images/nmr-labtour-poster.webp",
+        src: "assests/events_images/nmr-labtour-poster.webp",
         alt: "NMR Lab Tour Poster"
     },
     {
-        src: "/assests/events_images/curie_quiz_1.webp",
+        src: "assests/events_images/curie_quiz_1.webp",
         alt: "Curie Quiz"
     },
     {
-        src: "/assests/events_images/GreenTalk.webp",
+        src: "assests/events_images/GreenTalk.webp",
         alt: "Green Talk"
     },
     {
-        src: "/assests/events_images/chembio-talk-poster.webp",
+        src: "assests/events_images/chembio-talk-poster.webp",
         alt: "Chembio Talk Poster"
     },
     
