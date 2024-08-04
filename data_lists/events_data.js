f2456ed67b5d96dd0c5d3f6b78ba9b96a8517498
Curie Club IISERM <curieclub@iisermohali.ac.in>
2024-08-04 15:57:10 +0530

    Update events_data.js

--- data_lists/events_data.js
+++ data_lists/events_data.js
@@ -1,66 +1,66 @@
 export const upcomingEventList = [
-    {
-        eventName: "Tresure Hunt",
-        eventDate: "2022-05-15",
-        eventTime: "6:00 PM",
-        eventVenue: "Curie Museum",
-        eventDescription: "Find hidden treasures in the Curie Museum",
-        eventImage: "",
-    },
-    {
-        eventName: "Tresure Hunt",
-        eventDate: "2022-05-15",
-        eventTime: "6:00 PM",
-        eventVenue: "Curie Museum",
-        eventDescription: "Find hidden treasures in the Curie Museum",
-        eventImage: "",
-    },
-    {
-        eventName: "Tresure Hunt",
-        eventDate: "2022-05-15",
-        eventTime: "6:00 PM",
-        eventVenue: "Curie Museum",
-        eventDescription: "Find hidden treasures in the Curie Museum",
-        eventImage: "",
-    },
-    {
-        eventName: "Tresure Hunt",
-        eventDate: "2022-05-15",
-        eventTime: "6:00 PM",
-        eventVenue: "Curie Museum",
-        eventDescription: "Find hidden treasures in the Curie Museum",
-        eventImage: "",
-    },
-    {
-        eventName: "Tresure Hunt",
-        eventDate: "2022-05-15",
-        eventTime: "6:00 PM",
-        eventVenue: "Curie Museum",
-        eventDescription: "Find hidden treasures in the Curie Museum",
-        eventImage: "",
-    }
+    // {
+    //     eventName: "Tresure Hunt",
+    //     eventDate: "2022-05-15",
+    //     eventTime: "6:00 PM",
+    //     eventVenue: "Curie Museum",
+    //     eventDescription: "Find hidden treasures in the Curie Museum",
+    //     eventImage: "",
+    // },
+    // {
+    //     eventName: "Tresure Hunt",
+    //     eventDate: "2022-05-15",
+    //     eventTime: "6:00 PM",
+    //     eventVenue: "Curie Museum",
+    //     eventDescription: "Find hidden treasures in the Curie Museum",
+    //     eventImage: "",
+    // },
+    // {
+    //     eventName: "Tresure Hunt",
+    //     eventDate: "2022-05-15",
+    //     eventTime: "6:00 PM",
+    //     eventVenue: "Curie Museum",
+    //     eventDescription: "Find hidden treasures in the Curie Museum",
+    //     eventImage: "",
+    // },
+    // {
+    //     eventName: "Tresure Hunt",
+    //     eventDate: "2022-05-15",
+    //     eventTime: "6:00 PM",
+    //     eventVenue: "Curie Museum",
+    //     eventDescription: "Find hidden treasures in the Curie Museum",
+    //     eventImage: "",
+    // },
+    // {
+    //     eventName: "Tresure Hunt",
+    //     eventDate: "2022-05-15",
+    //     eventTime: "6:00 PM",
+    //     eventVenue: "Curie Museum",
+    //     eventDescription: "Find hidden treasures in the Curie Museum",
+    //     eventImage: "",
+    // }
 ]
 
 export const recentEventList = [
     {
         type: "Student Talk",
         eventName: "Introduction to the Computational Quantum Chemistry",
-        eventDescription: "Lorem ipsum dolor amet a restiae vitae?"
+        // eventDescription: "Lorem ipsum dolor amet a restiae vitae?"
     },
     {
         type: "What it all about?",
         eventName: " A cross-disciplinary approach towards solving the 'Global energy crisis",
-        eventDescription: "Lorem ipsum dolor sit."
+        // eventDescription: "Lorem ipsum dolor sit."
     },
     {
         type: "Quiz",
         eventName: "Guess The Element",
-        eventDescription: "Lorem ndae nulla molestiae vitae?"
+        // eventDescription: "Lorem ndae nulla molestiae vitae?"
     },
     {
         type: "Faculty Talk",
         eventName: "Biologiocal Phase Transitions",
-        eventDescription: "Lorem ipsum dolor sit amet dis illo amet a repudiandae nulla molestiae vitae?"
+        // eventDescription: "Lorem ipsum dolor sit amet dis illo amet a repudiandae nulla molestiae vitae?"
     },
 ]
 
@@ -71,10 +71,10 @@ export const onGoingSeriesList = [
         link: "htmls/molecule-of-the-week.html",
         lineClamp: 3
     },
-    {
-        seriesName: "Curieosity",
-        seriesDescription: "Check out our monthly article series where we delve into intriguing topics in chemistry. Want to write for Curiosity or suggest a topic? Get involved here",
-        link: "#",
-        lineClamp: 5
-    }
+    // {
+    //     seriesName: "Curieosity",
+    //     seriesDescription: "Check out our monthly article series where we delve into intriguing topics in chemistry. Want to write for Curiosity or suggest a topic? Get involved here",
+    //     link: "#",
+    //     lineClamp: 5
+    // }
 ]
