37ba1eb2ea744897985f333ace03f5ff647b79d7
Curie Club IISERM <curieclub@iisermohali.ac.in>
2024-08-04 22:42:46 +0530

    Add files via upload

---
+++ data_lists/quotes_data.js
@@ -0,0 +1,26 @@
+export const quotes = [
+    {
+        "quote": "Chemistry can be a good and bad thing. Chemistry is good when you make love with it. Chemistry is bad when you make crack with it.",
+        "credit": "Adam Sandler"
+    },
+    {
+        "quote": "Doing the same thing over and over again and expecting different results.",
+        "credit": "Albert Einstein"
+    },
+    {
+        "quote": "All that glitters may not be gold, but at least it contains free electrons.",
+        "credit": "John Desmond Bernal"
+    },
+    {
+        "quote": "Life without emotions is like an engine without fuel.",
+        "credit": "Mary Astor"
+    },
+    {
+        "quote": "Chemistry is a Mystery of Chemicals, Chemists are the Detectives who solve this Mystery.",
+        "credit": ""
+    },
+    {
+        "quote": "Life is chemistry; dilute your sorrow evaporate your worries filter your mistake boil your ego 'You will get the crystal of happiness'",
+        "credit": ""
+    }
+]
