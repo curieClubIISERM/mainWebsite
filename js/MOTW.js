1a60a82a08d02c2d0707a2217819053b13957a2d
Curie Club IISERM <curieclub@iisermohali.ac.in>
2024-08-04 16:11:55 +0530

    Update MOTW.js

--- js/MOTW.js
+++ js/MOTW.js
@@ -216,7 +216,7 @@ function updateContent(moleculeKey) {
 
     // Setup reference buttons
     const referencesButtons = document.querySelectorAll(".referencebtn");
-    const basePath = '/htmls/MOTW-reference.html';
+    const basePath = '/htmls/MOTW-references.html';
     referencesButtons.forEach((referenceButton) => {
         referenceButton.href = `${basePath}?molecule=${moleculeKey}`;
     });
