49a01350f2092c4ba33d60351e912439639fe504
Curie Club IISERM <curieclub@iisermohali.ac.in>
2024-08-21 18:31:48 +0530

    Update events_data.js

--- data_lists/events_data.js
+++ data_lists/events_data.js
@@ -1,7 +1,7 @@
 export const upcomingEventList = [
      {
          eventName: "INST Lab Tour",
-         eventDate: "2024-08-13",
+         eventDate: "Not Decided Yet",
          eventTime: "Not Decided Yet",
          eventVenue: "INST",
          eventDescription: " ",
