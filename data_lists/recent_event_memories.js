f13308a9a6da80db78b25e64d23f8071b6bc7725
Curie Club IISERM <curieclub@iisermohali.ac.in>
2024-09-27 22:52:43 +0530

    Update recent_event_memories.js

--- data_lists/recent_event_memories.js
+++ data_lists/recent_event_memories.js
@@ -1,5 +1,9 @@
 export const images = [
     {
+        src: "assests/events_images/Foundation_Day.webp",
+        alt: "Foundation Day "
+    },
+    {
         src: "assests/events_images/Curie_Treasure _Hunt.webp",
         alt: "Curie Treasure Hunt"
     },
@@ -10,10 +14,6 @@ export const images = [
     {
         src: "assests/events_images/INST_lab_tour.webp",
         alt: "INST Lab Tour"
-    },
-    {
-        src: "assests/events_images/guess_the_element.webp",
-        alt: "Guess the Element Poster"
     },
     {
         src: "assests/events_images/curie_quiz.webp",
