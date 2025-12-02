2b3e3e225742f53cb06953d618aeb951249f9f88
Ankit <158212441+Ankitk108@users.noreply.github.com>
2025-12-02 11:51:16 +0530

    Add G-form for Core Team 2025-26

--- data_lists/google_forms.js
+++ data_lists/google_forms.js
@@ -1,4 +1,6 @@
 export const googleFormLinks = [
-    { text: "Volunteer With Us", link: "https://forms.gle/2CpmTapmjUzrdDny5" , type: "Volunteering"},
+    { text: "Volunteer With Us", link: "https://forms.gle/2CpmTapmjUzrdDny5" , type: "Volunteering1"},
     { text: "Propose New Event", link: "https://forms.gle/veTG9qzu93Hnujke6" , type: "Proposing Event"},
+    { text: "Be Part of Core Team", link: "https://forms.gle/aSLmNFEK4djmZwUg6" , type: "Volunteering"},
+
   ];
