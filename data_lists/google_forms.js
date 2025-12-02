2df5a06f5e4ad2a4659ae01977a595193904d748
Ankit <158212441+Ankitk108@users.noreply.github.com>
2025-12-02 11:42:20 +0530

    Add new Google Form link for Core Team participation

--- data_lists/google_forms.js
+++ data_lists/google_forms.js
@@ -1,4 +1,5 @@
 export const googleFormLinks = [
     { text: "Volunteer With Us", link: "https://forms.gle/2CpmTapmjUzrdDny5" , type: "Volunteering"},
     { text: "Propose New Event", link: "https://forms.gle/veTG9qzu93Hnujke6" , type: "Proposing Event"},
+    { text: "Wanna be part of Core Team", link: "https://forms.gle/aSLmNFEK4djmZwUg6" , type: "Core Team"},
   ];
