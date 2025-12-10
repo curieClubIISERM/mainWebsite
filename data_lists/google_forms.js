d3ddeb33513cdfd602f355a775e0c4be0d4e8153
Ankit <158212441+Ankitk108@users.noreply.github.com>
2025-12-10 23:04:33 +0530

    Modify googleFormLinks for core team volunteering
    Updated the type for 'Be Part of Core Team' to 'CoreVolunteering'.

--- data_lists/google_forms.js
+++ data_lists/google_forms.js
@@ -1,6 +1,7 @@
 export const googleFormLinks = [
-    { text: "Volunteer With Us", link: "https://forms.gle/2CpmTapmjUzrdDny5" , type: "Volunteering1"},
+    { text: "Volunteer With Us", link: "https://forms.gle/2CpmTapmjUzrdDny5" , type: "Volunteering"},
     { text: "Propose New Event", link: "https://forms.gle/veTG9qzu93Hnujke6" , type: "Proposing Event"},
-    { text: "Be Part of Core Team", link: "https://forms.gle/aSLmNFEK4djmZwUg6" , type: "Volunteering"},
+    { text: "Be Part of Core Team", link: "https://forms.gle/aSLmNFEK4djmZwUg6" , type: "CoreVolunteering"},
+
 
   ];
